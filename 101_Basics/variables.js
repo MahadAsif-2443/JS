b3aa5b0dd875afae199f8408943ccc25ebf9b6e5
MahadAsif-2443 <148737286+MahadAsif-2443@users.noreply.github.com>
2024-09-14 11:57:43 +0000

    variables and also adding container config file with node js installed

---
+++ 101_Basics/variables.js
@@ -0,0 +1,13 @@
+// const can't be modified once it is declared
+const Id = "1234";                   
+// in JS we only have to use const and let and let can be modified even if declared before
+let name = "Mahad";
+// beware of using the var because of some issues arises in block and functional scope
+var college = "punjab group of colleges";
+// we can also assign memory this way without using any of the keyword but it is not recommended
+hobby = "programming";
+// if we dont gave any value to the variable then it will show an undefined in output
+
+
+//  Id = "454" it is not allowed because id is const and it cannot be modified
+console.log(Id);
