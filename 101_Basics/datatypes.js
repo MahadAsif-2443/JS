479e6bb28cfc9a6fdce0d4d8c6d304c3a1e77365
MahadAsif-2443 <148737286+MahadAsif-2443@users.noreply.github.com>
2024-09-15 09:09:20 +0000

    data types in JS primitive and object

---
+++ 101_Basics/datatypes.js
@@ -0,0 +1,35 @@
+// TWO MOST IMPORTANT DOCUMENTATION FOR ANY KIND OF HELP IN JS 1.TC39 AND 2.MDN
+
+"use strict";  //treat all the code of JS as newer version.
+
+// alert("i am pop-up box");  //as we are using node not the browser so that is not giving any output in node instead it gave an error
+
+// make the code readability your highest priority even that is ok in the language syntax.
+console.log("mahad")
+console.log(4+4);
+
+
+let name = "mahad"
+let age = 25
+let isLoggedin = true
+
+// Primitive Data types
+
+// number 2 => to power 53
+// bigint
+// string = ""
+// boolean = true/false
+// null  = it is an object type not primitive but also a standalone type and it is empty.
+// undefined  = currently value not assigned
+// symbol = represent uniqueness like when in react we have many components then we use symbol to show uniqueness
+
+// object data types
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
+
+
