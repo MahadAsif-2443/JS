d0561972b98062e78c6fcb6e04d95182d75255a7
MahadAsif-2443 <148737286+MahadAsif-2443@users.noreply.github.com>
2024-09-16 12:53:45 +0000

    datatype conversions

---
+++ 101_Basics/datatypesconversion.js
@@ -0,0 +1,41 @@
+// To convert the datatypes from one format to other
+
+//number to string
+let isnumber = 33
+console.log(typeof isnumber)
+console.log(isnumber)
+let isnumbertostring = String(isnumber)
+console.log(typeof(isnumbertostring))
+console.log(isnumbertostring)
+
+//string to number
+let isstring = "23"
+console.log(typeof isstring)
+console.log(isstring)
+let isstringtonumber = Number(isstring)
+console.log(typeof(isstringtonumber))
+console.log(isstringtonumber)
+
+//Undefined
+let isundefined = ""
+console.log(typeof isundefined)
+console.log(isundefined)
+let isundefinedtonumber = Number(isundefined)
+console.log(typeof(isundefinedtonumber))
+console.log(isundefinedtonumber)
+
+//Null to undefined
+let isnull = null
+console.log(typeof isnull)
+console.log(isnull)
+let isnulltostring = String(isnull)
+console.log(typeof(isnulltostring))
+console.log(isnulltostring)
+
+//Boolean to string
+let isboolean = true
+console.log(typeof isboolean)
+console.log(isboolean)
+let isbooleantostring = String(isboolean)
+console.log(typeof(isbooleantostring))
+console.log(isbooleantostring)
