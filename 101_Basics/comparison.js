cde5683304f7ca06f3d9cf8c573fe2f5ec2d8cda
MahadAsif-2443 <148737286+MahadAsif-2443@users.noreply.github.com>
2024-09-19 12:54:43 +0000

    comparison and conversion (problem in JS of convert data type automatically in comparison if both are not same data type)

---
+++ 101_Basics/comparison.js
@@ -0,0 +1,28 @@
+// Comparisons 
+// one of the most effecient comparisons is when we compare with similar data types they gave most probably accurate results.
+// all of these gave mostly accurate answers either in true or false.
+console.log(4 > 6)
+console.log(6 < 75)
+console.log(3 >= 2)
+console.log(5 <= 7)
+console.log(3 != 2)
+console.log(4 == 3)
+
+// JS, becomes headache when we do an comparisons between different data types.
+// they just convert the data types and then check for condition.
+console.log("3" < 6);
+console.log(null > 7);
+console.log(undefined == 6);
+
+// if we want the translator to check for equality and also not want to convert the data types then we use ===
+console.log(null === 5);
+
+// if you want to handle that disadvantage of JS profesionally then we use an TYPESCRIPT which provides
+// strictness in comparisons means it will not convert the datatype and directly check or do an comparison
+
+
+
+
+
+
+
