6340c1b8a5347b311b0deee3bc9518afef16fb37
MahadAsif-2443 <148737286+MahadAsif-2443@users.noreply.github.com>
2024-09-19 05:57:25 -0700

    Update comparison.js

--- 101_Basics/comparison.js
+++ 101_Basics/comparison.js
@@ -10,12 +10,13 @@ console.log(4 == 3)
 
 // JS, becomes headache when we do an comparisons between different data types.
 // they just convert the data types and then check for condition.
-console.log("3" < 6);
-console.log(null > 7);
-console.log(undefined == 6);
+console.log("3" < 6)
+console.log(null > 7)
+console.log(null >= 0)
+console.log(undefined == 6)
 
 // if we want the translator to check for equality and also not want to convert the data types then we use ===
-console.log(null === 5);
+console.log(null === 5)
 
 // if you want to handle that disadvantage of JS profesionally then we use an TYPESCRIPT which provides
 // strictness in comparisons means it will not convert the datatype and directly check or do an comparison
