9fc6762ae1b4ea5ab590811430f4996eceb9c71b
Mahad <148737286+MahadAsif-2443@users.noreply.github.com>
2025-10-11 23:21:53 +0000

    variables in javascript

---
+++ Basics/variables.js
@@ -0,0 +1,22 @@
+let info = "my name is mahad";
+console.log(info);
+info = 555;
+console.log(info);
+
+// const is refered as constant which is a value which cannot be changed
+const biggestnumber = 89;
+console.log(biggestnumber);
+// biggestnumber = 55;
+// console.log(biggestnumber);
+
+// let is another way to define variable and we can easily reassign it an many times as we want 
+let smallestnumber = 19;
+console.log(smallestnumber);
+smallestnumber = 5;
+console.log(smallestnumber);
+
+// var is also used to declare variable but have some issues regarding blocking scope
+var myname = "mahad";
+console.log(myname);
+
+// it is recommended by industry professionals to not use the var method to declare a variable.
