e544902858a074e1cc867f8e3a043fa870008efa
Mahad <148737286+MahadAsif-2443@users.noreply.github.com>
2025-10-16 17:44:50 +0000

    datatypes in js

---
+++ Basics/datatypes.js
@@ -0,0 +1,38 @@
+// There are two types of datatypes one is primitive data type and another is non primitive data type.
+
+// Primitive Datatypes
+// NNBBSSU
+
+// 1. Number
+let a = 67;
+
+//2. Null
+let b = null;
+
+// 3. BigInt
+let c = 782348888797868234867868348;
+
+// 4. Boolean
+let d = true;
+let e = false;
+
+// 5. String
+let f = "Mahad";
+
+// 6. Symbol
+// let i = symbol();
+
+// 7. undefined 
+let j = undefined;
+let k = '';
+
+// Non Primitive Datatype
+// object
+let persondetails = {
+    firstName : "Ahmed",
+    lastName : "Ijaz",
+    birthPlace : "Pakistan"
+};
+
+console.log(persondetails["firstName"]);
+
